297d966eca9eb85c08152a82e30e7fe081bca0a0
Ezhil1K <ezhil.1ek@hotmail.com>
2025-10-25 23:23:42 +0530

    Delete index.py

--- index.py
+++
@@ -1,7 +0,0 @@
-# This file is the required entry point for Vercel deployment.
-
-# Import the initialized Flask application instance from app.py
-from app import app
-
-# Explicitly define 'handler' for Vercel to reliably execute the application
-handler = app
